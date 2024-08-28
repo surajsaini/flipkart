fe59fa2ac8f8acb4b8feff6443cd487f65e0d9d5
Suraj Saini <s2008saini@gmail.com>
2024-08-28 13:37:24 +0530

    flipkartTestFIrstCommit

---
+++ SurajFresh/src/main/java/WebPage1/readPropFile.java
@@ -0,0 +1,35 @@
+package WebPage1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class readPropFile {
+	private Properties property;
+
+	public readPropFile() throws IOException {
+		property = new Properties();
+		InputStream input = new FileInputStream(
+				"C:\\SURAJ\\Backup_29122021\\Work\\Traning\\seleniumsep\\Fresh\\SurajFresh\\resources\\config.properties");
+		property.load(input);
+	}
+
+	public String getURL() {
+		String appURL = property.getProperty("URL");
+		return appURL;
+	}
+
+	public String getUsername() {
+		String username = property.getProperty("username");
+		return username;
+	}
+	
+	public String getBrowser() {
+		return property.getProperty("browser");
+	}
+	
+	public String getSearchMobile() {
+		return property.getProperty("searchmobile");
+	}
+}
